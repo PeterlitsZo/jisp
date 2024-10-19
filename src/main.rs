cae5388c34a67c5e65e4143b9c4cd71ae09b2320
Peterlits Zo <peterlitszo@gmail.com>
2024-10-19 19:23:54 +0800

    feat: Support read file as JISP and run & add e2e tests.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use std::{env, fs};
+
 mod token_stream;
 mod ast;
 mod asm;
 mod bytecode;
 
 fn main() {
-    let token_stream = token_stream::TokenStream::new(r###"
-        (+ 1 1)
-    "###);
+    let args: Vec<String> = env::args().collect();
+    let content = fs::read_to_string(&args[1]).unwrap();
+    let token_stream = token_stream::TokenStream::new(&content);
     let ast = ast::AstBuilder::new(token_stream).build();
     let asm = asm::AsmBuilder::new(ast).build();
     let bytecode = bytecode::BytecodeBuilder::new(asm).build();
---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+startup() {
+    cargo build --release
+}
+
+test() {
+    wanted="$1"
+    jisp_content="$2"
+    printf "%s" "$jisp_content" > /tmp/e2e_test.jisp
+    result=$(./target/release/jisp /tmp/e2e_test.jisp)
+    if [ "$result" = "$wanted" ]; then
+        echo "$jisp_content => $result"
+    else
+        echo "$jisp_content => $wanted expected, but got $result"
+        exit 1
+    fi
+}
+
+cleanup() {
+    rm /tmp/e2e_test.jisp
+}
+
+startup
+
+test 0 '0'
+test 1 '1'
+test 2 '2'
+
+test 2 '(+ 1 1)'
+test 3 '(+ 1 2)'
+test 350 '(+ 100 250)'
+
+cleanup
