a42adf385a82ea1f2c91387b48631551a02377a6
Peterlits Zo <peterlitszo@gmail.com>
2024-12-29 10:46:44 +0800

    feat(value): Support value Null and IFunc.

---
+++ src/value/mod.rs
@@ -0,0 +1,55 @@
+#![allow(dead_code)] // TODO (PeterlitsZo): This line need be removed in the future.
+
+//! Definded the type [Value] and its implementation.
+
+/// The Jisp value.
+#[derive(Debug, PartialEq, Clone)]
+enum Value {
+    Null,
+
+    IFunc(u32),
+}
+
+impl Value {
+    /// Get the null value.
+    fn null() -> Self {
+        Self::Null
+    }
+
+    /// Get the ifunc value by giving ifunc index.
+    fn ifunc(idx: u32) -> Self {
+        Self::IFunc(idx)
+    }
+
+    /// Check if the value is null and return `Some(())` if it is.
+    fn as_null(&self) -> Option<()> {
+        match self {
+            Self::Null => Some(()),
+            _ => None,
+        }
+    }
+
+    /// Check if the value is ifunc and return `Some(idx)` if it is.  Here
+    /// `idx` is the ifunc index in the Jisp program.
+    fn as_ifunc(&self) -> Option<u32> {
+        match self {
+            Self::IFunc(idx) => Some(*idx),
+            _ => None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_value_as_null() {
+        assert_eq!(Value::null().as_null(), Some(()));
+    }
+
+    #[test]
+    fn test_value_as_ifunc() {
+        assert_eq!(Value::ifunc(7).as_ifunc(), Some(7));
+    }
+}
