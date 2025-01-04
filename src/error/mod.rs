8219cd0eb526fda7787d452bd354e3830933c522
Peterlits Zo <peterlitszo@gmail.com>
2025-01-04 18:09:34 +0800

    feat(runner): Support op LoadInt, LoadFloat and Pop.

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,5 +1,7 @@
 use thiserror::Error;
 
+use crate::bytecode::Op;
+
 #[derive(Debug, Error)]
 pub(super) enum Error {
     /// The bytecode is empty - and cannot be run by runner.
@@ -9,6 +11,18 @@ pub(super) enum Error {
     /// Unknown opcode.
     #[error("unknown opcode: {0}")]
     UnknownOpcode(u8),
+
+    /// Bad opcode.
+    #[error("bad opcode: {0:?}")]
+    BadOpcode(Op),
+
+    /// There is no frame to run - bytecode is bad or internal error.
+    #[error("no frame to run")]
+    NoFrameToRun,
+
+    /// The stack is empty - and cannot pop.
+    #[error("empty stack")]
+    EmptyStack,
 }
 
 pub(super) type Result<T> = std::result::Result<T, Error>;
--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -62,7 +62,7 @@ impl<'r, 'b> FrameRunner<'r, 'b> {
     }
 
     fn run_frame(&mut self) -> Result<Value> {
-        let frame = self.runner.frames.last_mut().unwrap();
+        let frame = self.runner.frames.last_mut().ok_or(Error::NoFrameToRun)?;
         let ifunc = &self.bytecode.ifuncs()[frame.ifunc_idx];
 
         loop {
@@ -71,14 +71,34 @@ impl<'r, 'b> FrameRunner<'r, 'b> {
 
             match op {
                 Op::Return => {
-                    let value = frame.stack.pop().unwrap();
+                    let value = frame.stack.pop().ok_or(Error::EmptyStack)?;
                     self.runner.frames.pop();
                     return Ok(value);
                 }
+
                 Op::LoadNull => {
                     frame.stack.push(Value::null());
                 }
-                _ => unimplemented!(),
+                Op::LoadInt => {
+                    if ifunc.code().len() < frame.pc + 9 {
+                        return Err(Error::BadOpcode(op));
+                    }
+                    let val = &ifunc.code()[frame.pc + 1..frame.pc + 9];
+                    let val = i64::from_le_bytes(val.try_into().unwrap());
+                    frame.stack.push(Value::int(val));
+                }
+                Op::LoadFloat => {
+                    if ifunc.code().len() < frame.pc + 9 {
+                        return Err(Error::BadOpcode(op));
+                    }
+                    let val = &ifunc.code()[frame.pc + 1..frame.pc + 9];
+                    let val = f64::from_le_bytes(val.try_into().unwrap());
+                    frame.stack.push(Value::float(val));
+                }
+
+                Op::Pop => {
+                    frame.stack.pop().ok_or(Error::EmptyStack)?;
+                }
             }
             
             frame.pc += op.op_len();
@@ -99,6 +119,10 @@ mod tests {
         let program = indoc! { r#"
             ifunc 0 {
                 LOAD_NULL
+                POP
+                LOAD_INT        42
+                POP
+                LOAD_FLOAT      3.14
                 RETURN
             }
         "# };
@@ -106,6 +130,6 @@ mod tests {
         let bytecode = Bytecode::builder().parse_asm(&asm).build();
         let mut runner = Runner::new();
         let result = runner.run(&bytecode).unwrap();
-        assert_eq!(result, Value::null());
+        assert_eq!(result, Value::float(3.14));
     }
 }
